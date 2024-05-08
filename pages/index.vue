693ccc21457713417ad8f75398c231e8b5886d38
Ameer Hamza <ameer7hamza10@gmail.com>
2024-05-08 01:39:20 -0700

    header

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="flex px-10 w-full h-[4rem] bg-[white] shadow-sm border-b-[1px] items-center ">
+        <div class="h-full flex items-center hover:scale-105 transition-all duration-100 ease-in cursor-pointer">
+            <p class=" font-medium text-lg text-white bg-[#FF9F1C] py-2 px-3 rounded-md">Restaurants</p>
+            <p class=" font-medium text-lg py-3 px-1 rounded-md">Nearme</p>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
