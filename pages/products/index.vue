5d1fe01ea696c394a87995c0413f7b67ab2769ef
Ameer Hamza <ameer7hamza10@gmail.com>
2024-05-19 15:19:43 +0500

    remove code

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -57,7 +57,7 @@ const data = ref("");
 
 const press = async () => {
   axios
-    .get("https://restaurant-nearme.netlify.app/.netlify/functions/proxy")
+    .get("https://fascinating-daifuku-8a0ca0.netlify.app")
     .then((map) => {
       console.log(map.data);
       data.value = map.data;
